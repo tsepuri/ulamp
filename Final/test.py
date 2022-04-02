31d67e97a3b51729a1cf71805cabc1ba707e0c66
fedorenkood <fedorenkoalex23@gmail.com>
2022-04-03 00:48:40 +0100

    Test

--- Final/test.py
+++ Final/test.py
@@ -6,19 +6,33 @@ if multiple are available:
 e.g. cv2.CAP_MSMF or cv2.CAP_DSHOW.
 '''
 # open video0
-cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+cap = cv2.VideoCapture(0)
 # set width and height
 # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 # set fps
 # cap.set(cv2.CAP_PROP_FPS, 30)
-while(True):
+# while(True):
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     # Display the resulting frame
+#     # cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+for img_counter in range(2):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    print(ret)
     # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # cv2.imshow('frame', frame)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+    if ret:
+        name = "test"
+        print("Worked")
+        img_name = "dataset/"+ name +"/image_{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
